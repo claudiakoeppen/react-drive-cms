96f15f6f33f08fd0e4c627554a049108f2794ee5
Claudia Koeppen <crk42@duke.edu>
2020-05-17 16:19:17 -0400

    different id

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 window.appConf = {
     author: 'React Drive CMS',
-    dashboardId: '1NISpG3sfpLhnRlox5y8YGjHEgZuCmngNh-oyELHwtVk',
+    dashboardId: '2PACX-1vSpVJ_V0n46sfRUQbi3Dznf6hxsBWysCvfRHhoGT68IipgLyvTP_5sbSFlD5apCmRbLeos2Pn4dWwgI',
     sendContactMessageUrlId:
         'AKfycbxo-YNQD9a2BbJASiOt4Mj2WQbu5zuNJO4d5nV9o51_b5LxVO0',
     shortname: 'everythingproject',
